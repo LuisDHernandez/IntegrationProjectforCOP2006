8a506fe7e0c778c1a044ef367fc38aa05e9c04fa
LuisDHernandez <42774928+LuisDHernandez@users.noreply.github.com>
2018-12-05 19:20:36 -0500

    12/5 commit for IP
    Added JavaDocs in classes throughout project.
    .jar / .bat file created and now program can run outside the IDE

---
+++ Character.java
@@ -0,0 +1,25 @@
+public class Character {
+    /**
+     * @author LuisD.Hernandez
+     */
+
+	private String name;
+
+	public Character() {
+		// System.out.println("This is the default constructor!");
+		name = " not set";
+	}
+
+	public Character(String userName) {
+		// System.out.println("This is the overloaded constructor!");
+		userName = name;
+	}
+
+	public void setName(String userName) {
+		name = userName;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
